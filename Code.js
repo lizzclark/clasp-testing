ad96700f52b7fa8aa9d878f367194bf969c601dc
Lizz Clark <44777792+lizzclark@users.noreply.github.com>
2024-10-02 16:52:06 +0100

    pulled latest changes from web

--- Code.js
+++ Code.js
@@ -1,3 +1,5 @@
-function myFunction() {
-  
+function addToSheet() {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet()
+  const firstSheet = sheet.getSheetByName("Sheet1")
+  firstSheet.appendRow(["test", "hello world", new Date()])
 }
